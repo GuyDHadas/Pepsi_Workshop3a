b5856ceab56b895a43298580168c6d9656ed88ae
Tomer Saban <tomer.saban.42@gmail.com>
2020-12-31 16:28:37 +0200

    commit1

--- physics.py
+++ physics.py
@@ -112,3 +112,5 @@ def verlet_step(r_old, r, dt, L, rc ):
     F, virial = LJ_Forces(r, L, rc)
     r_new = 2 * r + F * dt**2 - r_old
     return r_new, virial
+
+
--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 import physics
 def test_LennardJonesForce():
-    print(physics.LennardJonesForce(100000, 5), "large distance")
-    print(physics.LennardJonesForce(2 ** (-1/6), 5), "minimum distance")
+    print(physics.LennardJonesForce(100000, 1000), "large distance")
+    print(physics.LennardJonesForce(0.001, 1000), "minimum distance")
+
+
+def test_LennardJonesPotential():
+    print(physics.LennardJonesPotential(2 ** (1/6), 1000), "need to be -1")
+
+
 
 
 test_LennardJonesForce()
