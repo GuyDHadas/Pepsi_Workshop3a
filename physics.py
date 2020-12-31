b0c240304eb40705e4b34da78f8a8f8b7c5260ae
Shadowguy1809 <guy.hadas01@gmail.com>
2020-12-31 15:14:15 +0200

    imports

--- physics.py
+++ physics.py
@@ -12,22 +12,29 @@ def running_time(func, x):
     return t_end - t_start
 
 
-# r_vec is a np. array of D elements (D is the number of dimensions ).
-# it is the vector which points from particle 1 to particle 2 (= r_ij =ri -rj)
+# r_vec is a np . array of D elements ( D is the number of dimensions ).
+# it is the vector which points from particle 1 to particle 2 (= r_ij = ri - rj )
 # this function returns the Lennard - Jones potential between the two particles
+@jit
 def LennardJonesPotential(r_vec, rc):
     r = np.linalg.norm(r_vec)  # calculate norm (= | r_ij |)
     if r > rc:
         return 0.
-    rc6 = rc * rc * rc * rc * rc * rc
+    rc2 = rc * rc
+    rc6 = rc2 * rc2 * rc2
     rc12 = rc6 * rc6
-    r6 = r * r * r * r * r * r
+    r2 = r * r
+    r6 = r2 * r2 * r2
     r12 = r6 * r6
     return 4 / r12 - 4 / r6 - 4 / rc12 + 4 / rc6
 
 
+print(LennardJonesPotential(np.linspace(0, 10, 10000), 3))
+
+
 # same as previous method but returns the force between the two particles
 # this is the gradient of the previous method
+@jit
 def LennardJonesForce(r_vec, rc):
     r = np.linalg.norm(r_vec)  # calculate norm (= | r_ij |)
     if r > rc:
@@ -35,18 +42,23 @@ def LennardJonesForce(r_vec, rc):
     y = 1 / r
     y2 = y * y
     y4 = y2 * y2
+    y6 = y4 * y2
     y8 = y4 * y4
-    return (48 * y8 * y8 * r * r - 24 * y8) * r_vec
-    # calculate the gradient of " LennardJonesPotential
+    y14 = y6 * y8
+    return (48 * y14 - 24 * y8) * r_vec
+
+
+print(LennardJonesForce(np.linspace(0, 10, 100000), 3))
 
 
 # this method calculates the total force on each particle
-# r is a 2D array where r[i ,:] is a vector with length D ( dimensions )
-# which represents the position of the i-th particle
-# (in 2D case r[i ,0] is the x coordinate and r[i ,1] is the y coordinate of the i-th
+# r is a 2 D array where r [i ,:] is a vector with length D ( dimensions )
+# which represents the position of the i - th particle
+# ( in 2 D case r [i ,0] is the x coordinate and r [i ,1] is the y coordinate of the i - th particle )
 # this function returns a numpy array F of the same dimensions as r
-# where F[i ,:] is a vector which represents the force that acts on the i-th particle
+# where F [i ,:] is a vector which represents the force that acts on the i - th particle
 # this function also returns the virial
+@jit
 def LJ_Forces(r, L, rc):
     F = np.zeros_like(r)
     virial = 0
@@ -63,6 +75,7 @@ def LJ_Forces(r, L, rc):
     return F, virial
 
 
+print(LJ_Forces(np.random.rand(100, 2), 1, 3))
 
 
 def system_energy(r_old, r, r_new, dt, L, rc):
