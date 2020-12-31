77406f4ef34404e339e8e0de192ffc169c69be3b
Tomer Saban <tomer.saban.42@gmail.com>
2020-12-31 17:00:39 +0200

    commit1

--- test.py
+++ test.py
@@ -1,13 +1,20 @@
 import physics
+import plot
+import dynamics
+
+
 def test_LennardJonesForce():
     print(physics.LennardJonesForce(100000, 1000), "large distance")
     print(physics.LennardJonesForce(0.001, 1000), "minimum distance")
 
 
 def test_LennardJonesPotential():
-    print(physics.LennardJonesPotential(2 ** (1/6), 1000), "need to be -1")
+    print(physics.LennardJonesPotential(2 ** (1 / 6), 1000), "need to be -1")
 
 
+def test_partical_drawing():
+    r_new = dynamics.T0_config(10 ** (-4), 5, 2)
+    plot.partical_drawing(r_new)
 
 
 test_LennardJonesForce()
